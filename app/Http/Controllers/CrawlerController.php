dd1ff2d61486d1197cbf55f12ad4aa2b685b3f02
Jean <jean@wj-ar.com>
2022-12-02 18:14:15 +0800

    initial commit

---
+++ app/Http/Controllers/CrawlerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Observers\Observer;
+use Spatie\Crawler\Crawler;
+
+class CrawlerController extends Controller
+{
+    //
+
+    public function crawl($url)
+    {
+    	Crawler::create()->set
+
+    }
+}
---
+++ app/Observers/Oberser.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Observers;
+
+use App\Models\Crawler;
+use Psr\Http\Message\UriInterface;
+
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
+use Spatie\Crawler\CrawlObservers\CrawlObserver;
+
+use Log;
+
+use Cache;
+
+/**
+ * 
+ */
+class Oberver extends CrawlObserver
+{
+	
+	
+}
+
+?>
